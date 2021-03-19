4e8b4808d4900e6c5714e7f0160de20390694ac8
scyking <scyking@hotmail.com>
2021-03-19 15:45:50 +0800

    添加图片选中效果

--- windows/images.go
+++ windows/images.go
@@ -22,12 +22,35 @@ func AddImageViewWidgets(path string, parent walk.Container) {
 	builder := NewBuilder(parent)
 
 	for _, name := range names {
+		var civ *walk.ImageView
 		iv := ImageView{
-			Image:   name,
-			Margin:  5,
-			MinSize: Size{120, 120},
-			MaxSize: Size{120, 120},
-			Mode:    ImageViewModeZoom,
+			AssignTo: &civ,
+			Name:     name,
+			Image:    name,
+			Margin:   5,
+			MinSize:  Size{120, 120},
+			MaxSize:  Size{120, 120},
+			Mode:     ImageViewModeZoom,
+			OnBoundsChanged: func() {
+				//civ.Background().Dispose()
+			},
+			OnMouseMove: func(x, y int, button walk.MouseButton) {
+				/*brush, err := walk.NewSolidColorBrush(walk.RGB(159, 215, 255))
+				if err != nil {
+					log.Fatal(err)
+				}
+				civ.SetBackground(brush)*/
+			},
+			OnMouseDown: func(x, y int, button walk.MouseButton) {
+				if button == walk.LeftButton {
+					ClearImageViewBackground(parent)
+					brush, err := walk.NewSolidColorBrush(walk.RGB(143, 199, 239))
+					if err != nil {
+						log.Fatal(err)
+					}
+					civ.SetBackground(brush)
+				}
+			},
 		}
 
 		if err := iv.Create(builder); err != nil {
@@ -35,4 +58,15 @@ func AddImageViewWidgets(path string, parent walk.Container) {
 		}
 	}
 
+}
+
+func ClearImageViewBackground(container walk.Container) {
+	widgets := container.Children()
+	if widgets != nil {
+		for i := widgets.Len() - 1; i >= 0; i-- {
+			if iv, ok := widgets.At(i).(*walk.ImageView); ok {
+				iv.SetBackground(walk.NullBrush())
+			}
+		}
+	}
 }
