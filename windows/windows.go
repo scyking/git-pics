602f23b71589fef88dff3f367750aa31d09a7576
scyking <scyking@hotmail.com>
2021-03-22 10:25:04 +0800

    解决radio button绑定默认值错误问题

--- windows/windows.go
+++ windows/windows.go
@@ -9,10 +9,17 @@ const (
 	FilePath
 )
 
-func DBSource() map[string]uint8 {
+// 数据绑定
+func DBSource() map[string]int {
 	// 数据绑定
-	db := make(map[string]uint8)
+	db := make(map[string]int)
 	// 设置text type默认类型
 	db[DBTextType] = FilePath
+
 	return db
+}
+
+// 根据配置生成“文件”path
+func Copy() error {
+	return nil
 }
