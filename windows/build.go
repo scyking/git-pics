803125acca65e84a6f2a59aa2d7515edc54d61fa
scyking <scyking@hotmail.com>
2021-04-02 14:44:57 +0800

    修改配置对话框布局

--- windows/build.go
+++ windows/build.go
@@ -208,11 +208,10 @@ func RunConfigDialog(owner walk.Form, cf *config.Config) (int, error) {
 	var db *walk.DataBinder
 	var acceptPB, cancelPB *walk.PushButton
 
-	ic := img.Shell32Icon(4)
-
 	return Dialog{
 		AssignTo:      &dlg,
 		Title:         "配置",
+		Icon:          img.Shell32Icon(69),
 		DefaultButton: &acceptPB,
 		CancelButton:  &cancelPB,
 		DataBinder: DataBinder{
@@ -221,37 +220,175 @@ func RunConfigDialog(owner walk.Form, cf *config.Config) (int, error) {
 			DataSource:     cf,
 			ErrorPresenter: ToolTipErrorPresenter{},
 		},
-		MinSize: Size{324, 200},
 		Layout:  VBox{},
+		MinSize: Size{380, 200},
 		Children: []Widget{
 			Composite{
 				Layout: Grid{Columns: 3},
 				Children: []Widget{
-					Label{
-						Text: "Workspace:",
+					GroupBox{
+						ColumnSpan: 3,
+						Title:      "Git信息",
+						Layout:     Grid{Columns: 3},
+						Children: []Widget{
+							Label{
+								Text: "Repository:",
+							},
+							LineEdit{
+								ColumnSpan: 2,
+							},
+							Label{
+								Text: "Server:",
+							},
+							LineEdit{
+								ColumnSpan: 2,
+							},
+							Label{
+								Text: "UserName:",
+							},
+							LineEdit{
+								ColumnSpan: 2,
+							},
+							Label{
+								Text: "Password:",
+							},
+							LineEdit{
+								ColumnSpan:   2,
+								PasswordMode: true,
+							},
+							Label{
+								Text: "Token:",
+							},
+							LineEdit{
+								ColumnSpan: 2,
+							},
+						},
 					},
-					LineEdit{
-						AssignTo: &le,
-						ReadOnly: true,
-						Text:     Bind("Workspace"),
+					GroupBox{
+						Title:      "快捷上传:",
+						ColumnSpan: 3,
+						Layout:     Grid{Columns: 3},
+						Children: []Widget{
+							Composite{
+								ColumnSpan: 3,
+								Layout:     HBox{},
+								Children: []Widget{
+									Label{
+										Text: "开启状态:",
+									},
+									Composite{
+										Layout: HBox{},
+										Children: []Widget{
+											RadioButtonGroup{
+												Buttons: []RadioButton{
+													{
+														Text: "ON",
+													},
+													{
+														Text: "OFF",
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+							Composite{
+								ColumnSpan: 3,
+								Layout:     HBox{},
+								Children: []Widget{
+									Label{
+										Text: "上传目录:",
+									},
+									LineEdit{
+										AssignTo: &le,
+										ReadOnly: true,
+										Text:     Bind("QuickDir"),
+									},
+									ToolButton{
+										Text:  "选择文件夹",
+										Image: img.Shell32Icon(4),
+										OnClicked: func() {
+											ws, err := OpenDir(mw, le.Text())
+											if ws == "" {
+												return
+											}
+
+											if err != nil {
+												mw.errMBox(err)
+											}
+
+											if err := le.SetText(ws); err != nil {
+												mw.errMBox(err)
+												return
+											}
+										},
+									},
+								},
+							},
+						},
 					},
-					ToolButton{
-						Text:  "选择文件夹",
-						Image: ic,
-						OnClicked: func() {
-							ws, err := OpenDir(mw, le.Text())
-							if ws == "" {
-								return
-							}
+					GroupBox{
+						Title:  "其他配置",
+						Layout: Grid{Columns: 3},
+						Children: []Widget{
+							Composite{
+								ColumnSpan: 3,
+								Layout:     HBox{},
+								Children: []Widget{
+									Label{
+										Text: "自动提交:",
+									},
+									Composite{
+										Layout: HBox{},
+										Children: []Widget{
+											RadioButtonGroup{
+												Buttons: []RadioButton{
+													{
+														Text: "ON",
+													},
+													{
+														Text: "OFF",
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+							Composite{
+								ColumnSpan: 3,
+								Layout:     HBox{},
+								Children: []Widget{
+									Label{
+										Text: "工作空间:",
+									},
+									LineEdit{
+										AssignTo: &le,
+										ReadOnly: true,
+										Text:     Bind("Workspace"),
+									},
+									ToolButton{
+										Text:  "选择文件夹",
+										Image: img.Shell32Icon(4),
+										OnClicked: func() {
+											ws, err := OpenDir(mw, le.Text())
+											if ws == "" {
+												return
+											}
 
-							if err != nil {
-								mw.errMBox(err)
-							}
+											if err != nil {
+												mw.errMBox(err)
+											}
 
-							if err := le.SetText(ws); err != nil {
-								mw.errMBox(err)
-								return
-							}
+											if err := le.SetText(ws); err != nil {
+												mw.errMBox(err)
+												return
+											}
+										},
+									},
+								},
+							},
 						},
 					},
 				},
@@ -265,10 +402,9 @@ func RunConfigDialog(owner walk.Form, cf *config.Config) (int, error) {
 						Text:     "OK",
 						OnClicked: func() {
 							if err := db.Submit(); err != nil {
-								log.Print(err)
+								mw.errMBox(err)
 								return
 							}
-
 							dlg.Accept()
 						},
 					},
